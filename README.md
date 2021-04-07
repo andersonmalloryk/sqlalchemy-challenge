8384bd7270191e5cc85c0dddd1aa43e8772e7eab
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-04-07 13:36:04 -0500

    Working on the max,min,avg summaries in FLASK

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ This is homework for the University of Minnesota's Data Analytics and Visualizat
 ## Methodology for climate analysis and data exploration
 Connect SQLAlchemy using 'create_engine'
 Use 'automap_base()' to reflect tables into classes and save a refrence called 'Station' and 'Measurement'
-Link Python to the database by creating a SQLAlchemy session
-Close out session at the end of the notebook
+Link Python to the database by creating a SQLAlchemy session. Close out session at the end of the notebook.
 
 ### Precipitation Analysis
 
@@ -17,8 +16,13 @@ Used Pandas to print the summary statistics for the precipitation data.
 
 ### Station Analysis
 
+Found the station that logged the most readings. Retrieved the last 12 months of readings by querying the 12 preceding months of data.
+
+Loaded query resutls into a Pandas DataFrame and set the index to the date column, sorted by 'date' and generated a histogram of the temperature results.
+
 Designed a query to calculate the total number of stations in the dataset to find the most active station. 
 
 ## Step 2 - Climate App
 
 Designed a Flask API based on the queries for precipitation and station analysis. 
+
---
+++ app.py
@@ -0,0 +1,139 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/20150823<br/>" 
+        f"/api/v1.0/20160823/20170823"  
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Convert query results to dictionary using date as key and prcp as value"""
+    # Query all passengers
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary
+    precipitation = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        precipitation.append(prcp_dict)
+
+    return jsonify(precipitation)
+
+
+@app.route("/api/v1.0/station")
+def station():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of station data"""
+    # Query stations
+    results = session.query(Station.name, Station.id, Station.station, Station.elevation, Station.latitude, Station.longitude).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list 
+    stations = []
+    for name, id, station, elevation, latitude, longitute in results:
+        station_dict = {}
+        station_dict["name"] = name
+        station_dict["id"] = id
+        station_dict["station"] = station
+        station_dict["elevation"] = elevation
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitute
+        stations.append(station_dict)
+
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    import datetime as dt
+
+    """Return a list of station data"""
+    recent_12_mo = dt.date(2017,8,23) - dt.timedelta(days = 365)
+    # Query dates and temperatures of most active station for last year    
+    recent = session.query(Measurement.date, Measurement.tobs, Measurement.station).filter(Measurement.date >= recent_12_mo).filter(Measurement.station == "USC00519281").all()
+
+    session.close()
+
+    recent_tobs = []
+    for date, tobs, station in recent:
+        recent_dict = {}
+        recent_dict["date"] = date
+        recent_dict["tobs"] = tobs
+        recent_dict["station"] = station
+        recent_tobs.append(recent_dict)
+    
+    return jsonify(recent_tobs)
+
+@app.route("/api/v1.0/20150823")
+def 20150823():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    import datetime as dt
+
+    """Return a list of station data"""
+    start_date = dt.date(2015,8,23)
+    # Find the max, min, and avg  
+    Start_tob_max = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= start_date).max()
+    Start_tob_avg = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= start_date).mean()
+    Start_tob_min = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= start_date).min()
+    
+    session.close()
+
+    start_tobs = {"TMIN": Start_tob_min, "TAVG": Start_tob_avg, "TMAX": Start_tob_max}
+    
+    return jsonify(start_tobs)
+
+@app.route("/api/v1.0/20160823/20170823")
+def 20160823/20170823():
+
+if __name__ == '__main__':
+    app.run(debug=True)
